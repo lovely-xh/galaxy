42295e8b3bc6bf800ace91ef03dd89f8da0dda22
lovely-xh <r_aider@qq.com>
2017-07-10 17:16:30 +0800

    Article

--- Article/src/com/galaxy/servlet/TestServlet.java
+++ Article/src/com/galaxy/servlet/TestServlet.java
@@ -8,27 +8,37 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import org.apache.log4j.Logger;
+import org.apache.log4j.PropertyConfigurator;
 
 public class TestServlet extends HttpServlet {
 	/**
 	 * 
 	 */
 	private static final long serialVersionUID = -8660331444039496229L;
-	
-//	private static Logger logger = Logger.getLogger(TestServlet.class);
+
+	private static Logger logger = Logger.getLogger(TestServlet.class);
 
 	public void init() {
-		
+		String prefix = getServletContext().getRealPath("/");
+		String location = getInitParameter("configfile");
+		PropertyConfigurator.configure(prefix + location);
 	}
-	
+
 	public void destroy() {
-		
+
 	}
-	
+
 	@Override
 	public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
 		PrintWriter pWriter = response.getWriter();
 		pWriter.print("hello fengliu");
 		pWriter.close();
+
+		logger.debug("test");
 	}
 }
+
+//log4jconfig.properties文件和当前运行的class放在同一目录下  
+//String connectdir=Test.class.getResource("log4jconfig.properties").getPath();  
+//PropertyConfigurator.configure(connectdir);  
+//System.out.println(connectdir);  
